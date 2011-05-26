4b251a122370034bd3b280ef72b40a7e64c8ed5b
maukoquiroga <mauko@pictorical.com>
2011-05-26 19:22:12 -0400

    to handle gmail confirmation

--- init.rb
+++ init.rb
@@ -35,7 +35,7 @@ module Interpres
     post '/emails' do
 #      begin
         req = Interpres::Sendgrid::ParseApi.new params
-        Email.create!(:from => req.from, :href => req.href).to_json
+        Email.create!(:from => req.from, :href => req.text).to_json
 #      rescue => e
 #        error 500, e.message.to_json
 #      end
