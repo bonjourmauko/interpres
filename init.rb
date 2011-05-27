b44bdf2c0f60751489685f2ec4d9d49c1e7fd80f
maukoquiroga <mauko@pictorical.com>
2011-05-26 20:01:55 -0400

    hoptoad without rack

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ require 'sendgrid'
 
 module Interpres  
   class Init < Sinatra::Base
-    use HoptoadNotifier::Rack
-    enable :raise_errors
+    #use HoptoadNotifier::Rack
+    #enable :raise_errors
     
     configure do
       env = ENV['SINATRA_ENV'] || 'development'
@@ -25,20 +25,22 @@ module Interpres
     end
     
     get '/emails/:id' do
-#      begin
+      begin
         Email.find(params[:id]).to_json
-#      rescue => e
-#        error 500, e.message.to_json
-#      end
+      rescue => e
+        HoptoadNotifier.notify e
+        error 500, e.message.to_json
+      end
     end
     
     post '/emails' do
-#      begin
+      begin
         req = Interpres::Sendgrid::ParseApi.new params
         Email.create!(:from => req.from, :href => req.href).to_json
-#      rescue => e
-#        error 500, e.message.to_json
-#      end
+      rescue => e
+        HoptoadNotifier.notify e
+        error 500, e.message.to_json
+      end
     end
         
   end
