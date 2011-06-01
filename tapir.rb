3d87e2a40e78bc777004825d5437aac7c601dcb1
maukoquiroga <mauko@pictorical.com>
2011-06-01 18:38:51 -0400

    shasha

--- tapir.rb
+++ tapir.rb
@@ -2,7 +2,7 @@ module Interpres
   module Tapir
   
     def self.send_source(params)
-      Nestful.send(:post, "http://tapir.heroku.com/sources", :params => params, :format => :json)
+      Nestful.send(:post, "http://tapir.heroku.com/sources", :params => params)
     end
     
   end
